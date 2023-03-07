1809e6b5052bbfc642d695dae685229a12dfd618
Clayton Wagner <claywest102@gmail.com>
2023-03-07 09:34:41 -0500

    Added login/register feature, issue with import statements tho

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['Secret_Key'] = 'ba79d314625ee40103217db43a6e536b'
 
 posts =[
     {
@@ -26,6 +29,24 @@ def home():
 def about():
     return render_template("about.html", title='About')
 
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'Success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register',form = form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data =='admin@blog.com' and form.password.data =='password':
+            flash('You have been logged in!', 'Success')
+            return redirect(url_for('home'))
+        else:
+            flash('Unsuccessful Login, check username and password', 'danger')
+    return render_template('Login.html', title='Login',form = form)
+
 if __name__ =='__main__':
     app.run(debug=True)
-    
